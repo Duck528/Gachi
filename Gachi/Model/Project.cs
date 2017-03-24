b6c8d90d8cdaab6b363d2b2da26b48d15aaf3709
Ahn DeockHwan <sdzaq@naver.com>
2017-03-25 04:18:32 +0900

    complete login, projec view. in progress join view

---
+++ Gachi/Model/Project.cs
@@ -0,0 +1,81 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gachi.Model
+{
+    public class Project : ObservableObject
+    {
+        private string title = "";
+        /// <summary>
+        /// 프로젝트 타이틀
+        /// </summary>
+        public string Title
+        {
+            get { return this.title; }
+            set
+            {
+                if (this.title != value)
+                {
+                    this.title = value;
+                    this.RaisePropertyChanged("Title");
+                }
+            }
+        }
+
+        private string createdAt = "";
+        /// <summary>
+        /// 프로젝트 생성 날짜
+        /// </summary>
+        public string CreatedAt
+        {
+            get { return this.createdAt; }
+            set
+            {
+                if (this.createdAt != value)
+                {
+                    this.createdAt = value;
+                    this.RaisePropertyChanged("CreatedAt");
+                }
+            }
+        }
+
+        private string lastModifiedAt = "";
+        /// <summary>
+        /// 마지막 수정 날짜
+        /// </summary>
+        public string LastModifiedAt
+        {
+            get { return this.lastModifiedAt; }
+            set
+            {
+                if (this.lastModifiedAt != value)
+                {
+                    this.lastModifiedAt = value;
+                    this.RaisePropertyChanged("LastModifiedAt");
+                }
+            }
+        }
+
+        private ObservableCollection<User> users = null;
+        /// <summary>
+        /// 이 프로젝트가 공유된 사용자 목록
+        /// </summary>
+        public ObservableCollection<User> Users
+        {
+            get { return this.users; }
+            set
+            {
+                if (this.users != value)
+                {
+                    this.users = value;
+                    this.RaisePropertyChanged("Users");
+                }
+            }
+        }
+    }
+}
---
+++ Gachi/Model/User.cs
@@ -0,0 +1,99 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gachi.Model
+{
+    public class User : ObservableObject
+    {
+        private string name = "";
+        /// <summary>
+        /// 사용자 이름
+        /// </summary>
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    this.RaisePropertyChanged("Name");
+                }
+            }
+        }
+
+        private string profileUrl = "";
+        /// <summary>
+        /// 사용자 프로필 이미지 경로
+        /// </summary>
+        public string ProfileUrl
+        {
+            get { return this.profileUrl; }
+            set
+            {
+                if (this.profileUrl != value)
+                {
+                    this.profileUrl = value;
+                    this.RaisePropertyChanged("ProfileUrl");
+                }
+            }
+        }
+
+        private string email = "";
+        /// <summary>
+        /// 사용자의 아이디로 사용되는 이메일 주소 (PK)
+        /// 이 값은 변경 불가능하다
+        /// </summary>
+        public string Email
+        {
+            get { return this.email; }
+            set
+            {
+                if (this.email != value)
+                {
+                    this.email = value;
+                    this.RaisePropertyChanged("Email");
+                }
+            }
+        }
+
+        private string nickName = "";
+        /// <summary>
+        /// 다른 사용자에게 보여지는 별명으로 식별키가 아니며,
+        /// 중복이 가능하고 변경이 가능하다
+        /// </summary>
+        public string NickName
+        {
+            get { return this.nickName; }
+            set
+            {
+                if (this.nickName != value)
+                {
+                    this.nickName = value;
+                    this.RaisePropertyChanged("NickName");
+                }
+            }
+        }
+
+        private string pw = "";
+        /// <summary>
+        /// 로그인에 사용되는 패스워드
+        /// </summary>
+        public string Pw
+        {
+            get { return this.pw; }
+            set
+            {
+                if (this.pw != value)
+                {
+                    this.pw = value;
+                    this.RaisePropertyChanged("Pw");
+                }
+            }
+        }
+    }
+}
