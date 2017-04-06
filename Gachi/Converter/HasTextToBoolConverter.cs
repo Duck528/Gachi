5cb3d1498af6bec030225d8af6451eaef05be154
Ahn DeockHwan <sdzaq@naver.com>
2017-04-07 02:24:50 +0900

    add send button in chat control

---
+++ Gachi/Converter/HasTextToBoolConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Gachi.Converter
+{
+    class HasTextToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var text = value as string;
+            if (text != null)
+            {
+                if ("".Equals(text.Trim()))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
